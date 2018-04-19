ab9abbfb553cee5644d4b097f418775589bc8c9d
curti2 <climbach@gmail.com>
2018-04-19 18:07:37 -0500

    modified:   week1-014.PositiveValue/nbproject/project.properties 	modified:   week1-014.PositiveValue/src/PositiveValue.java

--- mooc-2013-OOProgrammingWithJava-PART1/week1-014.PositiveValue/src/PositiveValue.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-014.PositiveValue/src/PositiveValue.java
@@ -5,6 +5,15 @@ public class PositiveValue {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type a number: ");
+        int firstnum = 10;
+        
+        if (firstnum < 0){
+            System.out.println("Your number is positive.");}
+        else{
+            System.out.println("Your number not positive.");
+        }
 
         // Type your program here:
     }
