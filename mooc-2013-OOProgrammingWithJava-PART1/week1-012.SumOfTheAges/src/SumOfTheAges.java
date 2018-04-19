43e1d9a9e3de8e80a8b4d8ab04449cbc6f67f989
curti2 <climbach@gmail.com>
2018-04-19 17:33:04 -0500

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch master
      Changes to be committed:
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-001.Name/nbproject/private/private.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-002.HelloWorld/nbproject/private/private.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-003.Spruce/nbproject/private/private.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-004.ChangingVariables/nbproject/private/private.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-005.SecondsOfTheYear/nbproject/private/private.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-006.Addition/nbproject/private/private.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-007.Multiplication/nbproject/private/private.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-008.Adder/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-008.Adder/nbproject/genfiles.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-009.Divider/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-009.Divider/nbproject/genfiles.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-010.CircleCircumference/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-010.CircleCircumference/nbproject/genfiles.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-011.BiggerNumber/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-011.BiggerNumber/nbproject/genfiles.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/nbproject/genfiles.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/nbproject/project.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-014.PositiveValue/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-014.PositiveValue/nbproject/genfiles.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-015.AgeOfMajority/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-015.AgeOfMajority/nbproject/genfiles.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-016.EvenOrOdd/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-016.EvenOrOdd/nbproject/genfiles.properties
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/nbproject/build-impl.xml
     	modified:   mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/nbproject/genfiles.properties

--- mooc-2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-012.SumOfTheAges/src/SumOfTheAges.java
@@ -5,6 +5,21 @@ public class SumOfTheAges {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+		System.out.print("Type your name: ");
+		String name1 = reader.nextLine();
+		
+		System.out.print("Type your age: ");
+		int age1 = Integer.parseInt(reader.nextLine());
+		
+		System.out.print("Type your name: ");
+		String name2 = reader.nextLine();
+		
+		System.out.print("Type your age: ");
+		int age2 = Integer.parseInt(reader.nextLine());
+		
+		int agesum = age1 + age2;
+		
+		System.out.println(name1 + " and " + name2 + " are " + agesum + " years old in total.");       
 
         // Implement your program here
     }
